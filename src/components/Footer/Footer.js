2bbe938c798467b78af938856ae4b5e6488b502a
CRM Crew <crmcrew2021@gmail.com>
2021-03-16 02:39:05 +0200

    :apple: completed footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,48 @@
 import React from 'react';
 
+import logo from '../../images/logo_blue.jpg';
 const Footer = () => {
   return (
     <div className='footer-container'>
       <div className='footer-container__main'>
-        <div>logo</div>
-        <div>content</div>
+        <div>
+          <img src={logo} alt='CodeDB Logo' />
+        </div>
+        <div className='footer-container__content'>
+          <div className='footer-container__content-row'>
+            <div>
+              <a href='#'>Career</a>
+            </div>
+            <div>
+              <a href='#'>About Us</a>
+            </div>
+            <div>
+              <a href='#'>Press Announcement</a>
+            </div>
+          </div>
+          <div className='footer-container__content-row'>
+            <div>
+              <a href='#'>Terms of Use</a>
+            </div>
+            <div>
+              <a href='#'>Privacy</a>
+            </div>
+          </div>
+          <div className='footer-container__content-row'>
+            <div>
+              <a href='#'>FAQs</a>
+            </div>
+            <div>
+              <a href='#'>Contact</a>
+            </div>
+            <div>
+              <a href='#'>Imprint</a>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='footer-container__copyright'>
-        Copy right 2021. CodeDB All Rights Reserved.
+        &copy; Copyright 2021. CodeDB All Rights Reserved.
       </div>
     </div>
   );
