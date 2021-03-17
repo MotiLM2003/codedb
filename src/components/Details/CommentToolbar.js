4f1fd3fe4c9ef45b6b4c580f20930f772df0b326
CRM Crew <crmcrew2021@gmail.com>
2021-03-17 04:09:48 +0200

    progress

--- src/components/Details/CommentToolbar.js
+++ src/components/Details/CommentToolbar.js
@@ -4,14 +4,14 @@ import comment from '../../images/comment.svg';
 const CommentToolbar = () => {
   return (
     <div className='flex gap-5'>
-      <div className='flex'>
-        <div>
+      <div className='flex '>
+        <div className='cursor'>
           <img src={heart} alt='Like button' />
         </div>
         <div>Likes</div>
       </div>
       <div className='flex'>
-        <div>
+        <div className='cursor'>
           <img className='test' src={comment} alt='Like button' />
         </div>
         <div>Comments</div>
