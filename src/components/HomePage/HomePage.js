685c91000a653acc88e1e200ae05018f4fa1b1bf
CRM Crew <crmcrew2021@gmail.com>
2021-03-20 22:45:31 +0200

    :apple: progress on home page screen - 2

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
                 the next project, you have your best solutions right at hand
                 without having to search for them.
               </p>
-              <div className='mt-82'>
+              <div className='mt-8'>
                 <button className='button yellow bg-primary'>Sign up</button>
               </div>
             </div>
@@ -37,11 +37,60 @@ const HomePage = () => {
       <section className='wrapper home-page__card flex align-items-center justify-center '>
         <div className='card w-1000 bg-white'>
           <div className=' flex gap-3 justify-space-around align-items-center grow'>
-            <div class='flex justify-center '>
-              <div className='bg-secondary w-485 rounded'>box</div>
+            <div class='flex justify-center  '>
+              <div className='home-page__card-item bg-variant-7  rounded'>
+                <div class='flex direction-column gap-05 align-items-center justify-center'>
+                  <div className='home-page__title color-black'>
+                    Learning a new
+                  </div>
+                  <div className='home-page__title color-black'>
+                    programming language?
+                  </div>
+                  <div className='home-page__title-small mt-2'>
+                    ...Then first orient yourself
+                  </div>
+                  <div className='home-page__title-small'>
+                    on the examples of others.
+                  </div>
+                  <div className='mt-4'>
+                    <button className='button bg-primary'>Start</button>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class='flex justify-center  '>
-              <div className='bg-secondary w-485 rounded'>box</div>
+              <div className='bg-secondary  rounded home-page__card-item'>
+                <div class='flex direction-column gap-05 align-items-center justify-center'>
+                  <div className='home-page__title color-white'>
+                    Learning a new
+                  </div>
+                  <div className='home-page__title color-white'>
+                    programming language?
+                  </div>
+                  <div className='home-page__title-small mt-2 color-white'>
+                    ...Then first orient yourself
+                  </div>
+                  <div className='home-page__title-small color-white'>
+                    on the examples of others.
+                  </div>
+                  <div className='mt-4'>
+                    <button className='button bg-primary'>Start</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='wrapper mt-14'>
+        <div className='home-page__hiw flex justify-center'>
+          <div className='flex direction-column'>
+            <div className='text-align-center font-family-montserrat font-size-56 font-weight-700 color-dark'>
+              How it works
+            </div>
+            <div className='text-align-center w-570 font-family-dm-sans font-size-20 mt-3'>
+              Quidam officiis similique sea ei, vel tollit indoctum efficiendi
+              ei, at nihil tantas platonem eos.
             </div>
           </div>
         </div>
